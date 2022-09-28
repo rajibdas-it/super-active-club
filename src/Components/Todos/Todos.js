679c437c9897bc0630cd52a074eeddb987f2b337
rajibdas-it <rajibrad@gmail.com>
2022-09-29 00:57:57 +0600

    not responsive

--- src/Components/Todos/Todos.js
+++ src/Components/Todos/Todos.js
@@ -13,6 +13,7 @@ const Todos = () => {
   return (
     <div className="w-[90%] mx-auto grid grid-cols-6 gap-4">
       <div className="todos-container col-span-4">
+        <p className="text-2xl font-bold mt-10">Super Active Club</p>
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
           {todos.map((todo) => (
             <Todo key={todo.id} todo={todo}></Todo>
@@ -20,15 +21,15 @@ const Todos = () => {
         </div>
       </div>
       <div className="cart-container bg-white col-span-2 shadow-xl">
-        <div className="info-section ml-2 flex mt-4">
+        <div className="info-section sm:ml-3 lg:ml-7 flex flex-col lg:flex-col items-center lg:justify-center mt-4">
           <img className="w-[50px] h-[50px] rounded-full" src={user} alt="" />
-          <div className="name-info ml-3">
+          <div className="name-info ml-4">
             <p className="font-semibold text-xl">Rajib Das</p>
             <p>Software Enginner</p>
           </div>
         </div>
-        <div className="others-info mt-7 w-[90%] mx-auto bg-slate-200 rounded-md p-3">
-          <div className="w-[80%] mx-auto flex gap-6 text-slate-600 font-semibold">
+        <div className="others-info mt-7 w-[80%] mx-auto bg-slate-200 rounded-md p-3">
+          <div className="w-[80%] mx-auto flex flex-col lg:flex-col justify-evenly gap-6 text-slate-600 font-semibold">
             <div className="weight">
               <p>
                 <span className="text-2xl font-bold text-black">85</span>kg
@@ -50,8 +51,8 @@ const Todos = () => {
           </div>
         </div>
         <div className="mt-6 ml-4">
-          <h1 className="font-bold text-slate-800">Add A Break</h1>
-          <div className="btn-group bg-slate-200 gap-2 w-[95%] p-3 rounded-md mt-3">
+          <h1 className="font-bold text-slate-800 ml-12">Add A Break</h1>
+          <div className="flex justify-evenly bg-slate-200 gap-2 w-[83%] mx-auto p-3 rounded-md mt-3">
             <button className="rounded-full bg-white p-2" value={10}>
               10s
             </button>
@@ -70,18 +71,18 @@ const Todos = () => {
           </div>
         </div>
         <div className="mt-6 ml-4">
-          <h1 className="font-bold text-slate-800">Working Details</h1>
-          <div className="btn-group bg-slate-200 gap-10 w-[95%] p-3 rounded-md mt-3">
+          <h1 className="ml-12 font-bold text-slate-800">Working Details</h1>
+          <div className="ml-12 bg-slate-200 gap-10 w-[83%] mx-auto flex sm:flex-row lg:flex-col justify-between p-3 rounded-md mt-3">
             <p>Exercise Time</p>
             <p>200 mins</p>
           </div>
-          <div className="btn-group bg-slate-200 gap-14 w-[95%] p-3 rounded-md mt-3">
+          <div className="ml-12 bg-slate-200 gap-14 w-[83%] mx-auto flex justify-between p-3 rounded-md mt-3">
             <p>Break Time</p>
             <p>200 mins</p>
           </div>
         </div>
-        <div className="mt-10 w-[80%] mx-auto">
-          <button className="btn btn-primary w-full text-white">
+        <div className=" mt-10 w-[80%] mx-auto text-center">
+          <button className="ml-3 btn btn-primary w-[80%] text-white">
             Activity Completed
           </button>
         </div>
