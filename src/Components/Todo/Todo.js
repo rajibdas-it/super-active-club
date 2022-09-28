b256c14f42f335324d819a20cfc294acbe035b02
rajibdas-it <rajibrad@gmail.com>
2022-09-28 23:51:13 +0600

    image added

---
+++ src/Components/Todo/Todo.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Todo = (props) => {
+  const { todo } = props;
+  const { id, img, title, description, time } = todo;
+  console.log(typeof time);
+  return (
+    <div className="card w-full bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10">
+        <img src={img} alt="Shoes" className="rounded-xl" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{title}</h2>
+        <p>{description}</p>
+        <p>
+          <small>Time Required: {time} mins</small>
+        </p>
+        <div className="card-actions">
+          <button className="btn btn-primary">Add to List</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
--- src/Components/Todos/Todos.js
+++ src/Components/Todos/Todos.js
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from "react";
+import Todo from "../Todo/Todo";
+import user from "./rajib.jpg";
 
 const Todos = () => {
   const [todos, setTodos] = useState([]);
   useEffect(() => {
     fetch("todos.json")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setTodos(data));
   }, []);
+  console.log(todos);
   return (
     <div className="w-[90%] mx-auto grid grid-cols-5 gap-4">
-      <div className="todos-container col-span-4 border border-red-400">
-        <div className="grid  sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
-          <h1>HI</h1>
-          <h1>HI</h1>
-          <h1>HI</h1>
+      <div className="todos-container col-span-4">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+          {todos.map((todo) => (
+            <Todo key={todo.id} todo={todo}></Todo>
+          ))}
         </div>
       </div>
       <div className="cart-container border border-green-400">
-        <h1>Hlw</h1>
+        <div className="info-section">
+          <img src={user} alt="" />
+        </div>
       </div>
     </div>
   );
