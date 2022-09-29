537fe5928b6dbfe272afe3cb3b238da782d3f192
rajibdas-it <rajibrad@gmail.com>
2022-09-29 12:04:32 +0600

    responsive done

--- src/Components/Others/Others.js
+++ src/Components/Others/Others.js
@@ -1,65 +1,76 @@
-<div className="info-section sm:ml-3 lg:ml-7 flex flex-col lg:flex-col items-center lg:justify-center mt-4">
-<img className="w-[50px] h-[50px] rounded-full" src={user} alt="" />
-<div className="name-info ml-4">
-  <p className="font-semibold text-xl">Rajib Das</p>
-  <p>Software Enginner</p>
-</div>
-</div>
-<div className="others-info mt-7 w-[80%] mx-auto bg-slate-200 rounded-md p-3">
-<div className="w-[80%] mx-auto flex flex-col lg:flex-col justify-evenly gap-6 text-slate-600 font-semibold">
-  <div className="weight">
-    <p>
-      <span className="text-2xl font-bold text-black">85</span>kg
-    </p>
-    <p>Weight</p>
-  </div>
-  <div className="height">
-    <p>
-      <span className="text-2xl font-bold text-black">5.7</span>
-    </p>
-    <p>Height</p>
-  </div>
-  <div className="age">
-    <p>
-      <span className="text-2xl font-bold text-black">31</span>yrs
-    </p>
-    <p>Age</p>
-  </div>
-</div>
-</div>
-<div className="mt-6 ml-4">
-<h1 className="font-bold text-slate-800 ml-12">Add A Break</h1>
-<div className="flex justify-evenly bg-slate-200 gap-2 w-[83%] mx-auto p-3 rounded-md mt-3">
-  <button className="rounded-full bg-white p-2" value={10}>
-    10s
-  </button>
-  <button className="rounded-full bg-white p-2" value={20}>
-    20s
-  </button>
-  <button className="rounded-full bg-white p-2" value={30}>
-    30s
-  </button>
-  <button className="rounded-full bg-white p-2" value={40}>
-    40s
-  </button>
-  <button className="rounded-full bg-white p-2" value={50}>
-    50s
-  </button>
-</div>
-</div>
-<div className="mt-6 ml-4">
-<h1 className="ml-12 font-bold text-slate-800">Working Details</h1>
-<div className="ml-12 bg-slate-200 gap-10 w-[83%] mx-auto flex sm:flex-row lg:flex-col justify-between p-3 rounded-md mt-3">
-  <p>Exercise Time</p>
-  <p>200 mins</p>
-</div>
-<div className="ml-12 bg-slate-200 gap-14 w-[83%] mx-auto flex justify-between p-3 rounded-md mt-3">
-  <p>Break Time</p>
-  <p>200 mins</p>
-</div>
-</div>
-<div className=" mt-10 w-[80%] mx-auto text-center">
-<button className="ml-3 btn btn-primary w-[80%] text-white">
-  Activity Completed
-</button>
-</div>
+// import React from 'react';
+
+// const Others = () => {
+//   return (
+//     <div>
+//       <div className="info-section sm:ml-3 lg:ml-7 flex flex-col lg:flex-col items-center lg:justify-center mt-4">
+// <img className="w-[50px] h-[50px] rounded-full" src={user} alt="" />
+// <div className="name-info ml-4">
+//   <p className="font-semibold text-xl">Rajib Das</p>
+//   <p>Software Enginner</p>
+// </div>
+// </div>
+// <div className="others-info mt-7 w-[80%] mx-auto bg-slate-200 rounded-md p-3">
+// <div className="w-[80%] mx-auto flex flex-col lg:flex-col justify-evenly gap-6 text-slate-600 font-semibold">
+//   <div className="weight">
+//     <p>
+//       <span className="text-2xl font-bold text-black">85</span>kg
+//     </p>
+//     <p>Weight</p>
+//   </div>
+//   <div className="height">
+//     <p>
+//       <span className="text-2xl font-bold text-black">5.7</span>
+//     </p>
+//     <p>Height</p>
+//   </div>
+//   <div className="age">
+//     <p>
+//       <span className="text-2xl font-bold text-black">31</span>yrs
+//     </p>
+//     <p>Age</p>
+//   </div>
+// </div>
+// </div>
+// <div className="mt-6 ml-4">
+// <h1 className="font-bold text-slate-800 ml-12">Add A Break</h1>
+// <div className="flex justify-evenly bg-slate-200 gap-2 w-[83%] mx-auto p-3 rounded-md mt-3">
+//   <button className="rounded-full bg-white p-2" value={10}>
+//     10s
+//   </button>
+//   <button className="rounded-full bg-white p-2" value={20}>
+//     20s
+//   </button>
+//   <button className="rounded-full bg-white p-2" value={30}>
+//     30s
+//   </button>
+//   <button className="rounded-full bg-white p-2" value={40}>
+//     40s
+//   </button>
+//   <button className="rounded-full bg-white p-2" value={50}>
+//     50s
+//   </button>
+// </div>
+// </div>
+// <div className="mt-6 ml-4">
+// <h1 className="ml-12 font-bold text-slate-800">Working Details</h1>
+// <div className="ml-12 bg-slate-200 gap-10 w-[83%] mx-auto flex sm:flex-row lg:flex-col justify-between p-3 rounded-md mt-3">
+//   <p>Exercise Time</p>
+//   <p>200 mins</p>
+// </div>
+// <div className="ml-12 bg-slate-200 gap-14 w-[83%] mx-auto flex justify-between p-3 rounded-md mt-3">
+//   <p>Break Time</p>
+//   <p>200 mins</p>
+// </div>
+// </div>
+// <div className=" mt-10 w-[80%] mx-auto text-center">
+// <button className="ml-3 btn btn-primary w-[80%] text-white">
+//   Activity Completed
+// </button>
+// </div>
+      
+//     </div>
+//   );
+// };
+
+// export default Others;
