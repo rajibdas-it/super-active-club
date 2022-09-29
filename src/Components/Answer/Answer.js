afbdb1176f5df37c6cbfb6f20a961a1d5d32616e
rajibdas-it <rajibrad@gmail.com>
2022-09-29 15:46:14 +0600

    added 2nd question

--- src/Components/Answer/Answer.js
+++ src/Components/Answer/Answer.js
@@ -5,18 +5,37 @@ const Answer = () => {
         <div>
             <div tabIndex={0} className="collapse group mb-2 rounded-md">
                 <div className="collapse-title bg-primary text-primary-content group-focus:bg-primary group-focus:text-secondary-content">
-                    How Does React Works?
+                    <p className='text-2xl font-bold'>How Does React Works?</p>
                 </div>
                 <div className="collapse-content bg-slate-300 text-primary-content group-focus:bg-slate-300 group-focus:text-secondary-content">
-                    <p>React create a virtual dom</p>
+                    <p className='mt-2'><span className='text-xl font-bold underline'>Declarative:</span> Create simple views for each state in application and react will efficiently update and render just the right components when data changes.</p>
+                    <p>Component Based: Application UI is wrapped inside a self-contained module known as a component. Components are used to define the visuals and interactions in applications.
+                    </p>
+                    <br />
+                    <p>
+                        <span className='text-xl font-bold underline'> Virtual DOM:</span> A virtual DOM object has similar properties as a real DOM object. It is a one-way data binding hence manipulating the virtual DOM is quick rather than updating the original DOM.
+                    </p>
+                    <br />
+                    <p>
+                        <span className='text-xl font-bold underline'>Event Handling:</span> React has a distinct way of handling events. Rather than directly targeting DOM events. React wraps them in their own event wrapper.
+                    </p>
+                    <br />
+                    <p>
+                        <span className='text-xl font-bold underline'>JavaScript extensions(JSX):</span> Using JSX while concise HTML/XML-like structures in the same file as you while javascript code then Babel will transform these expressions into actual JavaScript Code.
+                    </p>
+                    <br />
+                    <p>
+                        <span className='text-xl font-bold underline'>Performance:</span> React binds data with an application architecture called Flux controls. It helps us update the view for the user and with Flux can control the application workflow.
+                    </p>
+                    <br />
                 </div>
             </div>
             <div tabIndex={1} className="collapse group mb-2 rounded-md">
                 <div className="collapse-title bg-primary text-primary-content group-focus:bg-primary group-focus:text-secondary-content">
-                    What are the difference between Props and State?
+                    <p className='text-2xl font-bold'> What are the difference between Props and State?</p>
                 </div>
                 <div className="collapse-content bg-slate-300 text-primary-content group-focus:bg-slate-300 group-focus:text-secondary-content">
-                    <h3 className='text-xl font-bold underline'>Props</h3>
+                    <h3 className='mt-2 text-xl font-bold underline'>Props</h3>
                     <p>1. Props are read-only.</p>
                     <p>2. Props are immutable.</p>
                     <p>3. Props allow you to pass data from one component to other components as an argument.</p>
@@ -33,10 +52,10 @@ const Answer = () => {
             </div>
             <div tabIndex={3} className="collapse group mb-2 rounded-md">
                 <div className="collapse-title bg-primary text-primary-content group-focus:bg-primary group-focus:text-secondary-content">
-                    How Does React Works?
+                    <p className='text-2xl font-bold'> How many reasons we use useEffect without data loading?</p>
                 </div>
                 <div className="collapse-content bg-slate-300 text-primary-content group-focus:bg-slate-300 group-focus:text-secondary-content">
-                    <p>React create a virtual dom</p>
+                    <p className='mt-2'>React create a virtual dom</p>
                 </div>
             </div>
 
