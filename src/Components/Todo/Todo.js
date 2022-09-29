84929a9cb45a8c939cd50075caff90d3af43fbc6
rajibdas-it <rajibrad@gmail.com>
2022-09-29 20:51:47 +0600

    added a new todos in list

--- src/Components/Todo/Todo.js
+++ src/Components/Todo/Todo.js
@@ -4,8 +4,11 @@ const Todo = (props) => {
   const { todo, handleAddToList } = props;
   const { img, title, description, time } = todo;
   return (
-    <div className="card w-full bg-base-100 shadow-xl" data-aos="zoom-out-up"
-      data-aos-duration="1000">
+    <div
+      className="card w-full bg-base-100 shadow-xl"
+      data-aos="zoom-out-up"
+      data-aos-duration="1000"
+    >
       <figure className="px-10 pt-10">
         <img src={img} alt="Shoes" className="rounded-xl" />
       </figure>
@@ -16,7 +19,12 @@ const Todo = (props) => {
           <small>Time Required: {time} mins</small>
         </p>
         <div className="card-actions">
-          <button onClick={() => handleAddToList(todo)} className="btn btn-primary w-full">Add to List</button>
+          <button
+            onClick={() => handleAddToList(todo)}
+            className="btn btn-primary w-full"
+          >
+            Add to List
+          </button>
         </div>
       </div>
     </div>
